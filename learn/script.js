d7f2d73c7ee1aef97350228174076be6276a2d79
dainisstrumpe <danisstrumpe@startschool.org>
2026-01-12 16:46:44 +0200

    lerning jan12

---
+++ learn/script.js
@@ -0,0 +1,24 @@
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+
+    // 1️⃣ remove "active" from all tabs
+    tabs.forEach(t => t.classList.remove("active"));
+
+    // 2️⃣ add "active" to clicked tab
+    tab.classList.add("active");
+
+    // 3️⃣ get the tab key from data-tab
+    const tabKey = tab.dataset.tab;
+
+    // 4️⃣ hide all panels
+    panels.forEach(panel => panel.classList.toggle("active", panel.dataset.panel === tabKey));
+
+    // 5️⃣ show the panel with matching data-panel
+    // panels.forEach(panel => {
+    //     panel.classList.toggle("active", panel.dataset.panel === tabKey);
+    // })
+  });
+});
